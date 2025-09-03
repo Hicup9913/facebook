01648df98b8f540691144cabb5318148508f1541
Hicup9913 <Hicupsolanki@gmail.com>
2025-09-03 22:31:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/deploy"
+load_dotenv()
+
+DATABASE_URL = DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -16,4 +20,5 @@ def get_db():
         db.close()
 
 def create_tables():
+
     Base.metadata.create_all(bind=engine)
